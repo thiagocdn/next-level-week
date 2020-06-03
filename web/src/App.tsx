5da4c8641474f42a1062cc6bf78789a80f7eadaf
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-03 11:41:16 -0300

    Inserido rotas e iniciado pagina de criar ponto

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home';
+import Routes from './routes';
 
 function App() {
 
   return (
-    <Home />
+    <Routes />
   );
 }
 
---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreatePoint = () => {
+  return (
+    <h1>Hello Planet!</h1>
+  );
+};
+
+export default CreatePoint;
