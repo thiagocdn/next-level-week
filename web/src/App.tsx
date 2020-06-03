3bc89c1cd5f8ab41213f0887960d321b134b329f
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-03 11:11:13 -0300

    Iniciando criação da page Home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  
+
   return (
-    <div>
-      <Header title='Ecoleta' />
-      <h1>Conteudo da aplicação</h1>
-    </div>
+    <Home />
   );
 }
 
