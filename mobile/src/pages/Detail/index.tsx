073222069337592df285b26f02d858e65a5efd2f
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-04 15:27:19 -0300

    Adicionado informações dinamicas na pagina de details

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Image, Text, SafeAreaView } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
+
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point: {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+  items: {
+    title: string;
+  }[];
+}
 
 const Detail = () => {
+  const [data, setData] = useState<Data>({} as Data);
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(response => {
+      setData(response.data);
+    })
+  }, [])
 
   function handleNavigateBack(){
     navigation.goBack();
   }
+
+  if (!data.point) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
@@ -19,14 +53,17 @@ const Detail = () => {
           <Icon name='arrow-left' size={20} color='#34cb79' />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1540661116512-12e516d30ce4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
 
-        <Text style={styles.pointName}>Mercado do Zé</Text>
-        <Text style={styles.pointItems}>Lâmpadas, etc</Text>
+        <Text style={styles.pointName}>{data.point.name}</Text>
+          <Text style={styles.pointItems}>
+            {data.items.map(item => item.title).join(', ')}
+          </Text>
+        
 
         <View style={styles.address}>
           <Text style={styles.addressTitle}>Endereço</Text>
-          <Text style={styles.addressContent}>Valinhos, São Paulo</Text>
+          <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
         </View>
       </View>
 
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -76,8 +76,8 @@ const Points = () => {
     navigation.goBack();
   }
 
-  function handleNavigateToDetail(){
-    navigation.navigate('Detail')
+  function handleNavigateToDetail(id: number){
+    navigation.navigate('Detail', { point_id: id })
   }
 
   function handleSelectedItem(id: number) {
@@ -119,7 +119,7 @@ const Points = () => {
                 <Marker
                   key={String(point.id)}
                   style={styles.mapMarker}
-                  onPress={handleNavigateToDetail}
+                  onPress={() => handleNavigateToDetail(point.id)}
                   coordinate={{
                     latitude: point.latitude,
                     longitude: point.longitude,
