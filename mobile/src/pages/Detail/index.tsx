ea20fbf85d92331d054b631a472af591481a8e3a
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-04 14:20:27 -0300

    finalizado pagina de detalhes

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,123 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View, Image, Text, SafeAreaView } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  function handleNavigateBack(){
+    navigation.goBack();
+  }
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name='arrow-left' size={20} color='#34cb79' />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1540661116512-12e516d30ce4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
+
+        <Text style={styles.pointName}>Mercado do Zé</Text>
+        <Text style={styles.pointItems}>Lâmpadas, etc</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Valinhos, São Paulo</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name='whatsapp' size={20} color='#FFF' />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name='mail' size={20} color='#FFF' />
+          <Text style={styles.buttonText}>Email</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    paddingTop: 20,
+  },
+
+  pointImage: {
+    width: '100%',
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 10,
+    marginTop: 32,
+  },
+
+  pointName: {
+    color: '#322153',
+    fontSize: 28,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  pointItems: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  address: {
+    marginTop: 32,
+  },
+  
+  addressTitle: {
+    color: '#322153',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+
+  addressContent: {
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  footer: {
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: '#999',
+    paddingVertical: 20,
+    paddingBottom: 0,
+    paddingHorizontal: 32,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  
+  button: {
+    width: '48%',
+    backgroundColor: '#34CB79',
+    borderRadius: 10,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    marginLeft: 8,
+    color: '#FFF',
+    fontSize: 16,
+    fontFamily: 'Roboto_500Medium',
+  },
+});
 
 export default Detail;  
