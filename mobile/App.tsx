d5c7a9a2495148351ae08355cd2f10b980f2752a
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-04 11:19:31 -0300

    Adiciona as rotas para as paginas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { AppLoading } from 'expo';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
-import Home from './src/pages/Home'
+import Routes from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      <Home />
+      <Routes />
     </>
   );
 }
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
 import { StyleSheet, View, Image, ImageBackground, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native'
 
 import logo from '../../assets/logo.png';
 import background from '../../assets/home-background.png';
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate('Points');
+  }
+
   return (
   <ImageBackground
     source={background}
@@ -20,7 +27,7 @@ const Home = () => {
     </View>
 
     <View style={styles.footer}>
-      <RectButton style={styles.button} onPress={() => {}}>
+      <RectButton style={styles.button} onPress={handleNavigateToPoints}>
         <View style={styles.buttonIcon}>
           <Text>
             <Icon name='arrow-right' color='#FFF' size={24} />
