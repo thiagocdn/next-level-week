18886e6c081afed4ba2f1151412d9c1a09c04260
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-06-04 18:39:07 -0300

    Adicionado API do IBGE para estados e cidades

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,44 +1,142 @@
 import React, { useState, useEffect } from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
-import { StyleSheet, View, Image, ImageBackground, Text } from 'react-native';
+import { StyleSheet, View, Image, ImageBackground, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import RNPickerSelect from 'react-native-picker-select';
+import axios from 'axios';
 
 import logo from '../../assets/logo.png';
 import background from '../../assets/home-background.png';
 
+interface UF {
+  id: number;
+  nome: string;
+  sigla: string;
+}
+
+interface City {
+  id: number;
+  nome: string;
+  sigla: string;
+}
+
 const Home = () => {
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+  const [uf, setUF] = useState<UF[]>([]);
+  const [city, setCity] = useState<City[]>([]);
+
   const navigation = useNavigation();
 
+  useEffect(() => {
+    axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+      .then(response => {
+        setUF(response.data);
+      })
+  }, []);
+
+  useEffect(() => {
+    if(selectedUf === '0') {
+      return;
+    }
+    axios
+      .get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+      .then(response => {
+        setCity(response.data);
+      })
+  }, [selectedUf]);
+
+
   function handleNavigateToPoints() {
-    navigation.navigate('Points');
+    navigation.navigate('Points', {
+      selectedUf,
+      selectedCity,
+    });
+  }
+
+  function handleSelectedUf(uf: string){
+    setSelectedUf(uf);
+  }
+
+  function handleSelectedCity(city: string){
+    setSelectedCity(city);
   }
 
   return (
-  <ImageBackground
-    source={background}
-    style={styles.container}
-    imageStyle={{width: 274, height: 368}}
-  >
-    <View style={styles.main}>
-      <Image source={logo} />
-      <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-      <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-    </View>
-
-    <View style={styles.footer}>
-      <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-        <View style={styles.buttonIcon}>
-          <Text>
-            <Icon name='arrow-right' color='#FFF' size={24} />
-          </Text>
+    <KeyboardAvoidingView style={{ flex: 1 }}behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ImageBackground
+        source={background}
+        style={styles.container}
+        imageStyle={{width: 274, height: 368}}
+      >
+        <View style={styles.main}>
+          <Image source={logo} />
+          <View>
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+          </View>
         </View>
-        <Text style={styles.buttonText}>
-          Entrar
-        </Text>
-      </RectButton>
-    </View>
-  </ImageBackground>
+
+        <View style={styles.footer}>
+
+          <RNPickerSelect
+            placeholder={{
+              label: 'Selecione uma UF',
+              value: '0',
+            }}
+            style={{
+              viewContainer: {
+                backgroundColor: '#FFF',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 60,
+                borderRadius: 10,
+                marginBottom: 8,
+                paddingHorizontal: 24,
+              }
+            }}
+            onValueChange={item => handleSelectedUf(item)}
+            items={uf.map(item => {
+              return ({label: `${item.sigla} - ${item.nome}`, value: item.sigla})
+            })}
+          />
+      
+          <RNPickerSelect
+            placeholder={{
+              label: 'Selecione uma cidade',
+              value: '0',
+            }}
+            style={{
+              viewContainer: {
+                backgroundColor: '#FFF',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 60,
+                borderRadius: 10,
+                marginBottom: 8,
+                paddingHorizontal: 24,
+              }
+            }}
+            onValueChange={item => handleSelectedCity(item)}
+            items={city ? city.map(item => {
+              return ({label: `${item.nome}`, value: item.nome})
+            }): []}
+          />
+
+          <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name='arrow-right' color='#FFF' size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>
+              Entrar
+            </Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   )
 }
 
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -3,7 +3,7 @@ import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons'
 import { StyleSheet, View, Image, Text, ScrollView, Alert } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { SvgUri } from 'react-native-svg';
@@ -23,6 +23,11 @@ interface Point {
   longitude: number;
 }
 
+interface Params {
+  selectedUf: string;
+  selectedCity: string;
+}
+
 const Points = () => {
   const [ items, setItems ] = useState<Item[]>([]);
   const [ points, setPoints ] = useState<Point[]>([]);
@@ -30,6 +35,9 @@ const Points = () => {
   const [ initialPosition, setInitialPosition ] = useState<[number, number]>([0, 0]);
 
   const navigation = useNavigation();
+  const routes = useRoute();
+
+  const routeParams = routes.params as Params;
 
   useEffect(() => {
     async function loadPosition() {
@@ -63,14 +71,14 @@ const Points = () => {
   useEffect(() => {
     api.get('points', {
       params: {
-        city: 'Valinhos',
-        uf: 'SP',
-        items: [1, 2, 3, 4, 5, 6]
+        city: routeParams.selectedCity,
+        uf: routeParams.selectedUf,
+        items: selectedItems
       }
     }).then(response =>{
       setPoints(response.data);
     })
-  }, []);
+  }, [selectedItems]);
 
   function handleNavigateBack(){
     navigation.goBack();
